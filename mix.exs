19745340b3e3f9c8a746ebe50f15f88128e3b6c1
Jeremy Huffman <jeremy@jeremyhuffman.com>
2013-06-25 22:11:44 -0400

    update install task for Elixir 0.9.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Dialyxir.Mixfile do
 
   def project do
     [ app: :dialyxir,
-      version: "0.1.0",
+      version: "0.1.1",
       deps: deps
     ]
   end
@@ -22,15 +22,27 @@ defmodule Dialyxir.Mixfile do
 end
 defmodule Mix.Tasks.Install do
   use Mix.Task
-  @shortdoc "Locally install the Dialyzer tasks."
+  @shortdoc "Locally install the Dialyxir task archive."
 
   def run(_) do
+    case Code.ensure_loaded(Mix.Archive) do
+      {:module, _} -> do_install
+      {:error, _} -> do_old_install
+    end
+  end
+
+  defp do_install do
+      Mix.Tasks.Archive.run []
+      Mix.Tasks.Local.Install.run ["--force"]
+  end
+
+  defp do_old_install do
     cmds = "echo 'y' | mix local.install ebin/Elixir.Mix.Tasks.Dialyzer.beam"
     IO.puts cmds
-    IO.puts System.cmd(cmds) 
+    IO.puts System.cmd(cmds)
 
     cmds = "echo 'y' | mix local.install ebin/Elixir.Mix.Tasks.Dialyzer.Plt.beam"
     IO.puts cmds
-    IO.puts System.cmd(cmds) 
+    IO.puts System.cmd(cmds)
   end
 end
