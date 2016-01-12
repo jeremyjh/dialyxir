8945488a537fd43e86ef6d1bc59f579a082fe0b1
Jeremy Huffman <jeremy@jeremyhuffman.com>
2016-01-12 07:01:21 -0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dialyxir.Mixfile do
   def project do
     [
       app: :dialyxir,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.0",
       description: description,
       package: package,
