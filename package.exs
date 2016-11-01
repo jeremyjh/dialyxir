973020c1141275f38418c9fe2a1aaffa7b0b07f8
Jeremy Huffman <jeremy@jeremyhuffman.com>
2016-11-01 18:24:38 -0400

    remove expm support

--- package.exs
+++
@@ -1,5 +0,0 @@
-Expm.Package.new(name: "dialyxir", description: "Mix tasks to simplify use of Dialyzer in Elixir projects.",
-                 version: "0.2.2", keywords: ["dialyzer", "mix"],
-                 maintainers: [[name: "Jeremy Huffman",
-                                email: "jeremy@jeremyhuffman.com"]],
-                 repositories: [[github: "jeremyjh/dialyxir"]])
