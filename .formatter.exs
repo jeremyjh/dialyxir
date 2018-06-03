d9d1e1e203e4c0ffa959b45e555fd3ef044b86e4
Andrew Summers <oasummerso@gmail.com>
2018-06-03 18:14:19 -0500

    Add file to formatter list.

--- .formatter.exs
+++ .formatter.exs
@@ -1,9 +1,10 @@
 [
   inputs: [
+    "lib/dialyxir/formatter.ex",
     "lib/dialyxir/pretty_print.ex",
+    "lib/dialyxir/project.ex",
     "lib/dialyxir/warning.ex",
     "lib/dialyxir/warning_helpers.ex",
-    "lib/dialyxir/formatter.ex",
     "lib/dialyxir/warnings/*.ex"
   ]
 ]
