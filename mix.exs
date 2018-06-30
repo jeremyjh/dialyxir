78ecd452f07f10aab59b93312ccef34c3a760797
Jeremy Huffman <jeremy@jeremyhuffman.com>
2018-06-29 22:34:05 -0400

    bump version for rc.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Dialyxir.Mixfile do
   def project do
     [
       app: :dialyxir,
-      version: "1.0.0-rc.2",
+      version: "1.0.0-rc.3",
       elixir: ">= 1.6.0",
       description: description(),
       package: package(),
