4b1a57b6f46f7892a6bf2aafd29735ecf714e7a8
Jeremy Huffman <jeremy@jeremyhuffman.com>
2018-07-13 07:46:34 -0400

    app_call suggestion

--- lib/dialyxir/warnings/app_call.ex
+++ lib/dialyxir/warnings/app_call.ex
@@ -9,7 +9,7 @@ defmodule Dialyxir.Warnings.AppCall do
   @spec format_short([String.t()]) :: String.t()
   def format_short([module, function, arity, _culprit, _expected_type, _actual_type]) do
     pretty_module = Erlex.pretty_print(module)
-    "The call #{pretty_module}.#{function}/#{arity} has a type mismatch."
+    "Module or function to apply is not an atom: #{pretty_module}.#{function}/#{arity}."
   end
 
   @impl Dialyxir.Warning
