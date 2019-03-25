ffb5a0689886f3a37d89292e370ccc99821e5b5b
Jeremy Huffman <jeremy@jeremyhuffman.com>
2019-03-25 08:27:47 -0400

    Fix travis.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,6 +1,6 @@
 [
-  {":0:unknown_function Function :erl_types.t_is_opaque/1 does not exist."},
-  {":0:unknown_function Function :erl_types.t_to_string/1 does not exist."},
+  {":0:unknown_function Function t_is_opaque/1 does not exist."},
+  {":0:unknown_function Function t_to_string/1 does not exist."},
   ~r/unknown_type.*:wx.*/,
   ~r/format_(long|short).*no local return/
 ]
