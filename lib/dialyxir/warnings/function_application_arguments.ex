ba4a69bb70b514bdabb1eee995e510606bf97822
Tomas Janousek <tomi@nomi.cz>
2019-07-28 23:05:16 +0200

    Support OTP 22 in Dialyxir.Warnings.FuncionApplicationArguments
    > The format of the raw analysis result tagged with `fun_app_args` is
    > changed to `{fun_app_args, [ArgNs, Args, Type]}`.
    
    See:
    https://github.com/erlang/otp/blob/41bc2fccb1aa97e34e3cfc349c0b4b5d997b5256/lib/dialyzer/doc/src/notes.xml#L99
    https://github.com/erlang/otp/commit/6d9d4af8725f26972481b2e997c54ee5b3eafb29

--- lib/dialyxir/warnings/function_application_arguments.ex
+++ lib/dialyxir/warnings/function_application_arguments.ex
@@ -12,6 +12,11 @@ defmodule Dialyxir.Warnings.FuncionApplicationArguments do
     "Function application with #{pretty_args} will fail."
   end
 
+  # OTP 22+ format
+  def format_short([_arg_positions, args, type]) do
+    format_short([args, type])
+  end
+
   @impl Dialyxir.Warning
   @spec format_long([String.t()]) :: String.t()
   def format_long([args, type]) do
@@ -20,6 +25,27 @@ defmodule Dialyxir.Warnings.FuncionApplicationArguments do
 
     "Function application with arguments #{pretty_args} will fail " <>
       "since the function has type #{pretty_type}."
+  end
+
+  # OTP 22+ format
+  def format_long([arg_positions, args, type]) do
+    pretty_arg_positions = form_positions(arg_positions)
+    pretty_args = Erlex.pretty_print_args(args)
+    pretty_type = Erlex.pretty_print(type)
+
+    "Function application with arguments #{pretty_args} will fail " <>
+      "since the function has type #{pretty_type}, " <>
+      "which differs in #{pretty_arg_positions}."
+  end
+
+  defp form_positions(arg_positions = [_]) do
+    form_position_string = Dialyxir.WarningHelpers.form_position_string(arg_positions)
+    "the #{form_position_string} argument"
+  end
+
+  defp form_positions(arg_positions) do
+    form_position_string = Dialyxir.WarningHelpers.form_position_string(arg_positions)
+    "the #{form_position_string} arguments"
   end
 
   @impl Dialyxir.Warning
