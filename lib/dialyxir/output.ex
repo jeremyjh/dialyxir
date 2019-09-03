a7ebd805099288bcdbcfdbbdc17320ee2775e9c5
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2019-09-03 11:59:16 +0300

    Add Output test, fix hardcoded messages

--- lib/dialyxir/output.ex
+++ lib/dialyxir/output.ex
@@ -2,19 +2,8 @@ defmodule Dialyxir.Output do
   alias IO.ANSI
 
   def color(text, color) when is_binary(text) do
-    case atom_to_ansi(color) do
-      :error ->
-        text
-
-      {:ok, code} ->
-        ANSI.format([code, text, ANSI.reset()])
-    end
+    ANSI.format([color, text])
   end
-
-  defp atom_to_ansi(:red), do: {:ok, ANSI.red()}
-  defp atom_to_ansi(:yellow), do: {:ok, ANSI.yellow()}
-  defp atom_to_ansi(:green), do: {:ok, ANSI.green()}
-  defp atom_to_ansi(_), do: :error
 
   def info(""), do: :ok
   def info(text), do: Mix.shell().info(text)
--- test/dialyxir/dialyzer_test.exs
+++ test/dialyxir/dialyzer_test.exs
@@ -2,13 +2,31 @@ defmodule Dialyxir.DialyzerTest do
   use ExUnit.Case
   alias Dialyxir.Dialyzer
 
+  defmodule StubSuccess do
+    def run(_, _), do: {:ok, {"time", [], ""}}
+  end
+
+  defmodule StubWarn do
+    def run(_, _), do: {:ok, {"time", ["warning 1", "warning 2"], ""}}
+  end
+
+  defmodule StubError do
+    def run(_, _), do: {:error, "dialyzer failed"}
+  end
+
+  setup do
+    ansi_enabled = IO.ANSI.enabled?()
+
+    on_exit(fn ->
+      Application.put_env(:elixir, :ansi_enabled, ansi_enabled)
+    end)
+  end
+
   describe "dialyze/3" do
     import Dialyzer, only: [dialyze: 3]
 
-    test "formatting with no warnings" do
-      defmodule StubSuccess do
-        def run(_, _), do: {:ok, {"time", [], ""}}
-      end
+    test "formatting with no warnings, colors enabled" do
+      Application.put_env(:elixir, :ansi_enabled, true)
 
       {expected_result_code, expected_exit_code, expected_messages} =
         dialyze(nil, StubSuccess, nil)
@@ -19,14 +37,23 @@ defmodule Dialyxir.DialyzerTest do
       assert expected_messages == [
                "time",
                "",
-               [[[[], "\e[32m"], "done (passed successfully)"], "\e[0m"]
+               [[[[] | "\e[32m"], "done (passed successfully)"] | "\e[0m"]
              ]
     end
 
-    test "formatting with warnings" do
-      defmodule StubWarn do
-        def run(_, _), do: {:ok, {"time", ["warning 1", "warning 2"], ""}}
-      end
+    test "formatting with no warnings, colors disabled" do
+      Application.put_env(:elixir, :ansi_enabled, false)
+
+      {expected_result_code, expected_exit_code, expected_messages} =
+        dialyze(nil, StubSuccess, nil)
+
+      assert expected_result_code == :ok
+      assert expected_exit_code == 0
+      assert expected_messages == ["time", "", [[], "done (passed successfully)"]]
+    end
+
+    test "formatting with warnings, colors enabled" do
+      Application.put_env(:elixir, :ansi_enabled, true)
 
       {expected_result_code, expected_exit_code, expected_messages} = dialyze(nil, StubWarn, nil)
 
@@ -35,23 +62,48 @@ defmodule Dialyxir.DialyzerTest do
 
       assert expected_messages == [
                "time",
-               [[[[], "\e[31m"], "warning 1"], "\e[0m"],
-               [[[[], "\e[31m"], "warning 2"], "\e[0m"],
+               [[[[] | "\e[31m"], "warning 1"] | "\e[0m"],
+               [[[[] | "\e[31m"], "warning 2"] | "\e[0m"],
                "",
-               [[[[], "\e[33m"], "done (warnings were emitted)"], "\e[0m"]
+               [[[[] | "\e[33m"], "done (warnings were emitted)"] | "\e[0m"]
              ]
     end
 
-    test "formatting with errors" do
-      defmodule StubError do
-        def run(_, _), do: {:error, "dialyzer failed"}
-      end
+    test "formatting with warnings, colors disabled" do
+      Application.put_env(:elixir, :ansi_enabled, false)
+
+      {expected_result_code, expected_exit_code, expected_messages} = dialyze(nil, StubWarn, nil)
+
+      assert expected_result_code == :warn
+      assert expected_exit_code == 2
+
+      assert expected_messages == [
+               "time",
+               [[], "warning 1"],
+               [[], "warning 2"],
+               "",
+               [[], "done (warnings were emitted)"]
+             ]
+    end
+
+    test "formatting with errors, colors enabled" do
+      Application.put_env(:elixir, :ansi_enabled, true)
 
       {expected_result_code, expected_exit_code, expected_messages} = dialyze(nil, StubError, nil)
 
       assert expected_result_code == :error
       assert expected_exit_code == 1
-      assert expected_messages == [[[[[], "\e[31m"], "dialyzer failed"], "\e[0m"]]
+      assert expected_messages == [[[[[] | "\e[31m"], "dialyzer failed"] | "\e[0m"]]
+    end
+
+    test "formatting with errors, colors disabled" do
+      Application.put_env(:elixir, :ansi_enabled, false)
+
+      {expected_result_code, expected_exit_code, expected_messages} = dialyze(nil, StubError, nil)
+
+      assert expected_result_code == :error
+      assert expected_exit_code == 1
+      assert expected_messages == [[[], "dialyzer failed"]]
     end
   end
 end
