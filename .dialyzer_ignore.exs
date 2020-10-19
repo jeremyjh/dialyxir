039c38c44688356abab31f65362bb2e340421a10
Jeremy Huffman <jeremy@jeremyhuffman.com>
2020-10-18 21:26:52 -0400

    Bump travis versions.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,4 @@
 [
   {":0:unknown_function Function :erl_types.t_is_opaque/1 does not exist."},
-  {":0:unknown_function Function :erl_types.t_to_string/1 does not exist."},
-  ~r/format_(long|short).*no local return/
+  {":0:unknown_function Function :erl_types.t_to_string/1 does not exist."}
 ]
