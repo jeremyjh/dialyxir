5843d587859c5a320960a2374b2a18baf58c1637
Nick Krichevsky <nick@ollien.com>
2022-11-06 15:59:55 -0500

    Fix crash when mix.lock was missing
    Closes #472

--- lib/mix/tasks/dialyzer.ex
+++ lib/mix/tasks/dialyzer.ex
@@ -386,7 +386,23 @@ defmodule Mix.Tasks.Dialyzer do
     {apps, hash}
   end
 
-  def lock_file() do
-    Mix.Project.config()[:lockfile] |> File.read!()
+  defp lock_file() do
+    lockfile = Mix.Project.config()[:lockfile]
+    read_res = File.read(lockfile)
+
+    case read_res do
+      {:ok, data} ->
+        data
+
+      {:error, :enoent} ->
+        # If there is no lock file, an empty bitstring will do to indicate there is none there
+        <<>>
+
+      {:error, reason} ->
+        raise File.Error,
+          reason: reason,
+          action: "read file",
+          path: lockfile
+    end
   end
 end
---
+++ test/fixtures/no_lockfile/mix.exs
@@ -0,0 +1,10 @@
+defmodule NoLockfile.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :no_lockfile,
+      version: "1.0.0"
+    ]
+  end
+end
