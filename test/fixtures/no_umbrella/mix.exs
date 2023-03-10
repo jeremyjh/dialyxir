ed0df656a74eb6d4e906fe20fc056ca42ba7d5ec
pnezis <pnezis@gmail.com>
2023-03-10 13:55:06 +0200

    Add `:no_umbrella` config option
    In some cases you may want to place the `lockfile` in a parent folder
    without having an umbrella. With this extra option you avoid compilation
    at the parent folder which will fail since in such cases there is no
    `mix.exs`. If this flag is not set then the behaviour remains unchanged.

---
+++ test/fixtures/no_umbrella/mix.exs
@@ -0,0 +1,23 @@
+defmodule NoUmbrella.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :no_umbrella,
+      version: "0.1.0",
+      lockfile: "../mix.lock",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: [],
+      dialyzer: [no_umbrella: true]
+    ]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :public_key]]
+  end
+end
