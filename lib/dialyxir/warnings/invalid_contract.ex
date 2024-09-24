b69c5d895472059274c166258db5058ddef53f24
Jeremy Huffman <jeremy@jeremyhuffman.com>
2024-09-24 08:52:38 -0400

    Fix matching of invalid_contract args. (#541)

--- lib/dialyxir/warnings/invalid_contract.ex
+++ lib/dialyxir/warnings/invalid_contract.ex
@@ -32,6 +32,10 @@ defmodule Dialyxir.Warnings.InvalidContract do
   @impl Dialyxir.Warning
   @spec format_long([String.t()]) :: String.t()
   def format_long([module, function, arity, signature]) do
+    format_long([module, function, arity, nil, signature])
+  end
+
+  def format_long([module, function, arity, _args, signature | _]) do
     pretty_module = Erlex.pretty_print(module)
     pretty_signature = Erlex.pretty_print_contract(signature)
 
